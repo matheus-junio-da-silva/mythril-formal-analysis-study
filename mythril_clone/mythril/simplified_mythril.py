6656e4184ac53cb5bd88f635dac5723e0784f6db
matheus-junio-da-silva <matheusjuniodasilvapc@gmail.com>
2025-01-18 00:12:04 -0300

    it's working

---
+++ mythril_clone/mythril/simplified_mythril.py
@@ -0,0 +1,18 @@
+try:
+    #instancia de MythrilDisassembler
+    disassembler = MythrilDisassembler(
+        eth=None,
+        solc_version=None,
+        solc_settings_json=None,
+        solc_args=None,
+    )
+
+    # address = '0x0000000000000000000000000000000000000000' _ = [<mythril.solidity.soliditycontract.SolidityContract object at 0x7fe89273f070>]
+    address, _ = disassembler.load_from_solidity(
+        args.solidity_files
+    )  # list of files
+
+    execute_command(
+        disassembler=disassembler, address=address, parser=parser, args=args
+    )
+
